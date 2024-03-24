b9aa4215261ecaa4d9cf0f464a021f0f51eddc20
Tanawin Jenkunnaphat <tanawin_ohm@hotmail.com>
2024-03-24 22:51:17 +0700

    feat: first commit

---
+++ vite.config.js
@@ -0,0 +1,34 @@
+// Plugins
+import vue from '@vitejs/plugin-vue'
+
+// Utilities
+import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue({ 
+      template: {}
+    }),
+    // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+  ],
+  define: { 'process.env': {} },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+    extensions: [
+      '.js',
+      '.json',
+      '.jsx',
+      '.mjs',
+      '.ts',
+      '.tsx',
+      '.vue',
+    ],
+  },
+  server: {
+    port: 8080,
+  },
+})
